4e81edb889bf80afa8195b7ded7a0278c4058ac0
Adam Crockett <arc1224@gmail.com>
2019-02-26 15:53:37 -0800

    Finished react router project

---
+++ react-spa/src/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>Call us at: 123-456-7890</p>
+        <p>Email us at: bcs.contact@bcs.org</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
